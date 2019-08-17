06fd47cc0256286e6c74aaf8bbcd01f409b96528
Shoaib <msk4862@gmail.com>
2019-08-17 23:30:01 +0530

    fixes

--- week1_programming_challenges/1_sum_of_two_digits/APlusB.cpp
+++ week1_programming_challenges/1_sum_of_two_digits/APlusB.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int sum_of_two_digits(int first_digit, int second_digit) {
     return first_digit + second_digit;
 }
@@ -7,8 +9,8 @@ int sum_of_two_digits(int first_digit, int second_digit) {
 int main() {
     int a = 0;
     int b = 0;
-    std::cin >> a;
-    std::cin >> b;
-    std::cout << sum_of_two_digits(a, b);
+    cin >> a;
+    cin >> b;
+    cout << sum_of_two_digits(a, b);
     return 0;
 }
