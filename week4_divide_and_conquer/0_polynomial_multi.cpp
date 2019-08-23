382a9ff3e7b83abbb3e4e755f8cd58373a321861
Shoaib <msk4862@gmail.com>
2019-08-23 22:42:24 +0530

    poly

--- week4_divide_and_conquer/0_polynomial_multi.cpp
+++ week4_divide_and_conquer/0_polynomial_multi.cpp
@@ -23,6 +23,22 @@ vector<int> multiply_naive(vector<int> &a, vector<int> &b) {
 	return res;
 }
 
+int multiply_fast(vector<int> &a, vector<int> &b,int n, int a_low, int b_low) {
+	int size = 2*n-1;
+	vector<int> res(size);
+
+	if (n == 1) 
+	{
+		res[0] = a[a_low]*b[b_low];
+		return res[0];
+	}
+
+	cout<<multiply_fast(a, b, n/2, 0, 0)<< " ";
+	cout<< multiply_fast(a, b, n/2, n/2, n/2)<< " ";
+
+	return -1;
+}
+
 int main() {
 	int n;
 	cin>>n;
