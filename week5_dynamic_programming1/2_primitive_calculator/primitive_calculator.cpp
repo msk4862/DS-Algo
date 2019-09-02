ae017d3f823ba7ba1dd24b3930de78fc2633e58a
Shoaib <msk4862@gmail.com>
2019-09-02 17:03:45 +0530

    primitive calc - optimal

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -4,6 +4,76 @@
 
 using std::vector;
 
+vector<int> optimal_sequence_correct(int n) {
+  vector<int> minMoves(n+1);
+  int Move1, Move2, Move3;
+
+  //Calculating optimal moves till n
+  minMoves[0] = 0;
+  minMoves[1] = 0;
+
+  for (int num = 2; num <= n; ++num)
+  {
+    Move1 = 99999999;
+    Move2 = 99999999;
+    Move3 = 99999999;
+
+    Move1 = minMoves[num - 1] + 1;
+    if (num % 2 == 0) {
+      Move2 =  minMoves[ num / 2] + 1; 
+    }
+    if (num % 3 == 0) {
+      Move3 =  minMoves[num / 3] + 1;
+    }
+
+    minMoves[num] = (Move1 < Move2) ? ((Move1 < Move3) ? Move1 : Move3) : 
+                                      ((Move2 < Move3) ? Move2 : Move3);
+  }
+
+
+  //Finding moves of nth number
+  int number = n;
+  vector<int> sequence;
+  sequence.push_back(number);
+  
+  while(number != 1) {
+    Move1 = 99999999;
+    Move2 = 99999999;
+    Move3 = 99999999;
+
+    Move1 = minMoves[number-1];
+    if (number % 2 == 0) {
+      Move2 =  minMoves[ number / 2]; 
+    }
+    if (number % 3 == 0) {
+      Move3 =  minMoves[number / 3];
+    }
+
+    int minMove = (Move1 < Move2) ? ((Move1 < Move3) ? Move1 : Move3) : 
+                                      ((Move2 < Move3) ? Move2 : Move3);
+
+    if (minMove == Move1)
+    {
+      number = number -1;
+    }
+    else if (minMove == Move2)
+    {
+      number = number / 2;
+    }
+    else {
+      number = number / 3;
+    }
+
+    sequence.push_back(number);
+  }
+
+  //inserting value of minimum operations
+  sequence.push_back(minMoves[n]);
+  reverse(sequence.begin(), sequence.end());
+  return sequence;
+}
+
+
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence;
   while (n >= 1) {
@@ -23,9 +93,9 @@ vector<int> optimal_sequence(int n) {
 int main() {
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
+  vector<int> sequence = optimal_sequence_correct(n);
+  std::cout << sequence[0]<< std::endl ;
+  for (size_t i = 1; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
   }
 }
