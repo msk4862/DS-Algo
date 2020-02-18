109269860939719a208846f457d9ce0eb342a99b
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-02-18 23:35:36 +0530

    bit manip

--- Practice/UVA-OnlineJudge/num_contains_all_digit.cpp
+++ Practice/UVA-OnlineJudge/num_contains_all_digit.cpp
@@ -28,34 +28,4 @@ int main() {
         
     }
     
-}#include<bits/stdc++.h>
-using namespace std;
-
-#define ll long long int
-
-int main() {
-    ll t, n, temp, bits = 0;
-
-    cin>>t;
-
-    while (t--)
-    {
-        cin>>n;
-
-        temp = n;
-
-        while (temp)
-        {
-            bits = bits | 1<<(temp%10);
-            temp/=10;
-        }
-
-        if (bits == (1<<10)-1) {
-            printf("Contains all digit\n");
-        } else {
-            printf("False\n");
-        }
-        
-    }
-    
 }
