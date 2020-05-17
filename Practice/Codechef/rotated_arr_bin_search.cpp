8beaa0534ccd5e62e73c58f83360d6be22945fc5
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-17 21:37:33 +0530

    bin search on rotated array

---
+++ Practice/Codechef/rotated_arr_bin_search.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bin_search(vector<int> a, int key) {
+    int start = 0;
+    int end = a.size()-1;
+
+    // array is divided into two sorted arrays
+    while(start <= end) {
+        int mid = (end-start)/2 + start;
+
+        if(key == a[mid]) {
+            return mid;
+        }
+
+        else if(a[start] <= a[mid]) {
+            // In first sorted array partiton
+            if(key > a[start] && key < a[mid]) {
+                end = mid-1;
+            } else {
+                end = mid+1;
+            }
+        }
+        else {
+            // In other half of sorted array partiton
+            if(key > a[mid] && key < a[end]) {
+                end = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        int e;
+        cin>>e;
+        a.push_back(e);
+    }
+
+    int key;
+    cin>>key;
+    cout<<bin_search(a, key);
+    
+    
+}
