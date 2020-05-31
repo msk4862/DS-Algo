4fba97fe2b110a412d24cefa2171fd503a6eb99f
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-31 20:12:58 +0530

    linked list merge and cycle

---
+++ DataStructures/LinkedLst/findMergePoint.cpp
@@ -0,0 +1,31 @@
+int findMergeNode(Node* head1, Node* head2) {
+    SinglyLinkedListNode* head_org1 = head1;
+    SinglyLinkedListNode* head_org2 = head2;
+
+    int l1 = getLength(head1), l2 = getLength(head2);
+    
+    int diff = abs(l1-l2);
+
+    //syncing for longer list
+    if(l1 < l2) {
+        while (diff!=0) {
+            head_org2 = head_org2->next;
+            diff--;
+        }
+    } else if(l1 > l2) {
+        while (diff!=0) {
+            head_org1 = head_org1->next;
+            diff--;
+        }
+    }
+    
+    while(head_org1 != head_org2) {
+        
+            head_org1 = head_org1->next;
+            head_org2 = head_org2->next;   
+
+    }
+
+    return head_org1->data;
+
+}
--- DataStructures/LinkedLst/singlylist_cycle_detection.cpp
+++ DataStructures/LinkedLst/singlylist_cycle_detection.cpp
@@ -1,15 +1,20 @@
 bool has_cycle(SinglyLinkedListNode* head) {
+     if(head==nullptr) {
+        return false;
+    }
+
     SinglyLinkedListNode* slow = head;
     SinglyLinkedListNode* fast = head;
 
-    while(fast->next && fast->next->next) {
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
         if(fast == slow) {
             return true;
-        } else {
-            slow = slow->next;
-            fast = fast->next->next;
         }
     }
+
 return false;
+
 
 }
