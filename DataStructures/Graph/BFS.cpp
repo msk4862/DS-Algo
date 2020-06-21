575062bf86a6e21a02a7ef7604af0626601a8e5e
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-06-21 17:28:19 +0530

    graph bfs

---
+++ DataStructures/Graph/BFS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class Graph {
+    map<T, list<T> > l;
+
+    public:
+        void addEdge(T s, T d) {
+            l[s].push_back(d);
+            l[d].push_back(s);
+        }
+        
+        void bfs(T src) {
+            map<T, bool> visited;
+            queue<T> q;
+
+            q.push(src);
+            visited[src] = true;
+            while(!q.empty()) {
+                T v = q.front();
+
+                // tranverse neighbours of current node and push into queue and mark visited
+                for(auto nbr:l[v]) {
+                    if(!visited[nbr]) {
+                        q.push(nbr);
+                        visited[nbr] = true;
+                    }
+                }
+
+                // remove currently visited vertex
+                cout<<v<<", ";
+                q.pop();
+            } 
+        }
+};
+
+int main() {
+    Graph<int> g;
+
+    g.addEdge(0,1);
+    g.addEdge(0,3);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+    g.addEdge(3,4);
+    g.addEdge(4,5);
+
+    g.bfs(0);
+
+    return 0;
+}
