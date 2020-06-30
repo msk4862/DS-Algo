eeb0881a53c5d89d2eb16a71e6d079c58f621426
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-07-01 00:02:54 +0530

    week 4 day22 leet

---
+++ Practice/LeetCode/30DayChallenge/week4/Day22.cpp
@@ -0,0 +1,37 @@
+/*
+  Subarray Sum Equals K
+
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        long sum = 0;
+        int l = nums.size();
+        
+        unordered_map<int, int> sumFreq;
+        int count=0;
+        
+        sumFreq[sum] = 1;
+        for(int i = 0; i < l; ++i) {
+            sum += nums[i];
+            
+            // sum-i = k, i = sum-k (i's are cumulative sums stored in map)
+            // so we have to find if i  already exist
+            if(sumFreq.count(sum-k) > 0) {
+                count+=sumFreq[sum-k];
+            }
+            
+            if(sumFreq.count(sum)> 0)
+                sumFreq[sum]+=1;
+            else
+                sumFreq[sum]=1;
+            
+            
+        }
+        
+        // for(auto x:sumFreq) cout<<x.first<<" ";
+        
+        return count;
+    }
+};
