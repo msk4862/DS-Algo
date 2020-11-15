30897cc79b350c4164383af21e7f9001c9f2deb9
msk4862 <msk4862@gmail.com>
2020-11-15 20:17:23 +0530

    sort leetcode  probs

---
+++ Practice/LeetCode/Problems/179.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool comp(int a, int b) {
+        string sa = to_string(a);
+        string sb = to_string(b);
+        
+        if(sa.size() == sb.size()) return sa > sb;
+        // concat
+        return sa + sb > sb + sa ;
+    }
+    
+    string largestNumber(vector<int>& nums) {
+        string res = "";
+        
+        sort(nums.begin(), nums.end(), comp);   
+        // all zeroes
+        if(nums[0] == 0) return "0";
+        
+        for(int num : nums) {
+            // cout<<num<<" ";
+            res += to_string(num);
+        }
+        
+        return res;
+    }
+};
