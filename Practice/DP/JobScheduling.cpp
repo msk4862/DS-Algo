e301d6dd1a42bacc69357a4c619ae69a0830a0bc
msk4862 <msk4862@gmail.com>
2020-12-08 01:31:33 +0530

    dp2

--- Practice/DP/JobScheduling.cpp
+++ Practice/DP/JobScheduling.cpp
@@ -11,6 +11,10 @@ using namespace std;
 
 int dp[100];
 
+bool comp(int jobA[], int jobB[]) {
+	return jobA[1] < jobB[1];
+}
+
 // next job postion that does not overlap with current  nth job
 int nonOverlappingJob(int jobs[][3], int n) {
 	for (int i = n-1; i >= 0; i -= 1) {
@@ -32,11 +36,32 @@ int getMaxProfitTD(int jobs[][3], int n) {
 	// including current job	
 	int p2 = jobs[n-1][2];
 
-	int nextPossibleJob = nonOverlappingJob(jobs, n);
-	if(nextPossibleJob != -1)
-		p2 += getMaxProfitTD(jobs, nextPossibleJob);
+	int nextCompatibleJob = nonOverlappingJob(jobs, n);
+	if(nextCompatibleJob != -1)
+		p2 += getMaxProfitTD(jobs, nextCompatibleJob);
 	
 	return dp[n-1] = max(p1, p2);
+}
+
+int getMaxProfitBU(int jobs[][3], int n) {
+	int dp[n];
+	// base case
+	dp[0] = jobs[0][2];
+	
+	for(int i = 1; i < n; ++i) {
+		// excluding cur job
+		int p1 = dp[i-1];
+		
+		// including current job
+		int p2 = jobs[i][2];
+		int compatibleJob =  nonOverlappingJob(jobs, i+1);
+		if(compatibleJob != -1)
+			p2 += dp[compatibleJob];  
+			
+		dp[i] = max(p1, p2);
+	}
+		
+	return dp[n-1];
 }
 
 void solve() {   
@@ -49,9 +74,14 @@ void solve() {
 	for (int i = 0; i < n; i += 1) {
 		cin>>jobs[i][0]>>jobs[i][1]>>jobs[i][2];
 	}
+
+	// sort by finishing time
+	// to easily find compatible job	
+	sort(jobs, jobs + n, comp);
 	
 	memset(dp, -1, sizeof(dp));
-	cout<<getMaxProfitTD(jobs, n);
+	cout<<getMaxProfitTD(jobs, n)<<endl;
+	cout<<getMaxProfitBU(jobs, n);
 }
 
 int main() {
---
+++ Practice/DP/matrixChainMul.cpp
@@ -0,0 +1,73 @@
+// https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FASTIO \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr); \
+    cout.tie(nullptr);
+
+int cost[100][100];
+
+// dimension of nth MAT = MAT[i-1] * MAT[i]
+// put braces fom i  to j
+int getMinMultOprTD(int mat[], int open, int  close) {
+    // matrix invalid 
+    if(open == close) return 0;
+    
+    if(cost[open][close] != -1) return cost[open][close];
+    
+    cost[open][close] = INT_MAX;
+    // put brackets one by one place
+    for (int bracket = open; bracket < close; bracket += 1) {
+    	// oprs in cur Multiply matrices = (AB)(CD) = row(A)*col(B)*col(D)
+    	int curOprs =  getMinMultOprTD(mat, open, bracket) + getMinMultOprTD(mat, bracket+1, close) + mat[open-1]*mat[bracket]*mat[close];
+        cost[open][close] = min(cost[open][close], curOprs);
+    }
+    
+    return cost[open][close];
+}
+
+
+int getMinMultOprBU(int mat[], int n) {
+   int cost[n][n]; 
+   
+   for (int i = 0; i < n; i += 1) {
+        cost[i][i] = 0;
+   }
+   
+  for (int k = 0; k < n-1; k += 1) {
+    // start = 1 -> beacuse mat[start-1] will not be valid for 0
+  	for (int start = 1, end = start + k+1; end < n; start++, end++) {
+  		
+  		cost[start][end] = INT_MAX;
+  		for (int bracket = start; bracket < end; bracket += 1) {
+  			 int curOprs = cost[start][bracket] + cost[bracket+1][end] + mat[start-1]*mat[bracket]*mat[end];
+  		     cost[start][end] = min(cost[start][end], curOprs);
+  		}
+  	}
+  }
+   
+   return cost[1][n-1];
+}
+
+void solve() {   
+	int n;
+	cin>>n;
+	
+	int mat[n];
+	for (int i = 0; i < n; i += 1) {
+		cin>>mat[i];
+	}
+	
+	memset(cost, -1, sizeof(cost));
+	cout<<getMinMultOprTD(mat, 1, n-1)<<endl;
+	cout<<getMinMultOprBU(mat, n)<<endl;
+}
+
+int main() {
+    FASTIO
+    solve();
+    return 0;
+}
