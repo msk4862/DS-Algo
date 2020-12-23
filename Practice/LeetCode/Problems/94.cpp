d913a82637e09fb0590bb5587fc227a7376457a3
msk4862 <msk4862@gmail.com>
2020-12-24 00:54:19 +0530

    leetcode bt

--- Practice/LeetCode/Problems/94.cpp
+++ Practice/LeetCode/Problems/94.cpp
@@ -11,59 +11,32 @@
  */
 class Solution {
 public:
-    vector<int> res;
-    void pre(TreeNode* r) {
-        if(!r) return;
-        res.push_back(r->val);
-        pre(r->left);
-        pre(r->right);
-    }
-    
-    vector<int> preorderTraversal(TreeNode* root) {
-        pre(root);
-        return res;
-    }
-};
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> inorderTraversal(TreeNode* root) {
         if(!root) return {};
         
         vector<int> res;
         stack<TreeNode*> st;
         
         do {
+            // left subtree
             while(root) {
-                // root
-                res.push_back(root->val);
-                
-                // left subtree
                 st.push(root);
-                
+                cout<<root->val<<" ";
+
                 root = root->left;
             }
             
+            // root
             TreeNode* cur = st.top();
             st.pop();
+            res.push_back(cur->val);
             
             // right subtree
             if(cur->right) {
                 root = cur->right;
             }
         }while(!st.empty() || root);
-            
+    
         return res;
     }
 };
