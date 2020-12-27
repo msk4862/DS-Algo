2c84eaf133bf22dd71287ca136a8be2ade6e5a62
msk4862 <msk4862@gmail.com>
2020-12-27 17:01:12 +0530

    leetcode bt

--- Practice/LeetCode/Problems/114.cpp
+++ Practice/LeetCode/Problems/114.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     
-    TreeNode* flattenH(TreeNode* root) {
+    TreeNode*  flattenH(TreeNode* root) {
         if(!root) return nullptr;
         
         TreeNode* orgRight = root->right;
@@ -27,8 +27,47 @@ public:
         root->left = nullptr;
         return root;
     }
+    
+    // head, tail
+    pair<TreeNode*,TreeNode*>  flattenH1(TreeNode* root) {
+        if(!root) return { nullptr, nullptr};
+        
+        // single node
+        if(!root->left && !root->right) return {root, root};
+
+        // if left is null and right is not null
+        if(!root->left && root->right) {
+            auto right = flattenH1(root->right);
+            
+            root->right = right.first;
+            return {root, right.second};
+        }
+
+        // if left is not null and right is null
+        if(root->left && !root->right) {
+            auto left = flattenH1(root->left);
+            
+            root->right = left.first;
+            root->left = nullptr;
+            return {root, left.second};
+        }
+                
+        
+        // if both left and right subtree exist
+        auto left = flattenH1(root->left);
+        auto right = flattenH1(root->right);
+        
+        cout<<left.first->val<<" ";
+        
+        root->right = left.first;
+        left.second->right = right.first;
+        
+        root->left = nullptr;
+        return { root, right.second};
+    }
+    
         
     void flatten(TreeNode* root) {
-        flattenH(root);
+        flattenH1(root);
     }
 };
