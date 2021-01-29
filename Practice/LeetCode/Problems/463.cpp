63202a11e52a0b84314321b724c26ab1c01294cc
msk4862 <msk4862@gmail.com>
2021-01-30 00:28:25 +0530

    LC 463

--- Practice/LeetCode/Problems/463.cpp
+++ Practice/LeetCode/Problems/463.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    int traverse(vector<vector<int>>& grid, int i, int j) {
-        if(i<0 or j<0 or i >= grid.size() or j >= grid[0].size() or grid[i][j] == 0) return 1;
-            
-        // travelled
-        if(grid[i][j] == -1)
-            return 0;
+    int islandPerimeterHelper(vector<vector<int>>& grid, int r, int c) {
+        if(r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == 0) return 1;
+        if(grid[r][c] == -1) return 0;
         
-            cout<<i<<j<<" ";
-        
-        grid[i][j] = -1;
-        return traverse(grid, i-1, j) + traverse(grid, i+1, j) + traverse(grid, i, j+1) + traverse(grid, i, j-1);
+        grid[r][c] = -1;
+        return islandPerimeterHelper(grid, r-1, c) + islandPerimeterHelper(grid, r, c-1) + islandPerimeterHelper(grid, r+1, c) + islandPerimeterHelper(grid, r, c+1);
     }
     
     int islandPerimeter(vector<vector<int>>& grid) {
-        int ans = 0, f=0;
+        
         for(int i = 0; i < grid.size(); ++i) {
-            for(int j = 0; j < grid[0].size(); ++j) {
-                if(grid[i][j]) {
-                    ans = traverse(grid, i, j);
-                    f = 1;
-                    break;
-                }
+            for(int j = 0; j< grid[0].size(); ++j) {
+                if(grid[i][j] == 1)
+                    return islandPerimeterHelper(grid, i, j);
             }
-            if(f) break;
-            
         }
         
-        return ans;
+        return -1;
     }
 };
