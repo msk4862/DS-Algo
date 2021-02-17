97f86a158a405907c1a26d450bd4b02a409a8082
msk4862 <msk4862@gmail.com>
2021-02-17 22:53:03 +0530

    LC 39

--- Practice/LeetCode/Problems/39.cpp
+++ Practice/LeetCode/Problems/39.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    
-    void subset_sum(vector<int> a, int tar, vector<vector<int>>& res, int start = 0, vector<int> cur = {}, int sum = 0) {
-        
-        if(sum == tar) {
-            res.push_back(cur);
+    // same as subset sum problem
+    void combSumHelper(vector<int>& ar, int ind, int tar, vector<vector<int>>& res, vector<int> cur = {}) {
+        if(ind == ar.size()) {
+            if(tar == 0)  res.push_back(cur);
             return;
         }
         
-        for (int i = start; i < a.size(); i ++) {
-            if(sum + a[i] <= tar) {
-                cur.push_back(a[i]);
-                sum+=a[i];
-                subset_sum(a, tar, res, i, cur, sum);
-                sum -= cur[cur.size()-1];
+        if(tar >= ar[ind]) {
+            // include
+            cur.push_back(ar[ind]);
+            combSumHelper(ar, ind, tar-ar[ind], res, cur);            
+            
+            // exclude (backtrack)
+            cur.pop_back();
+            combSumHelper(ar, ind+1, tar, res, cur);
+        }
+        // can't inlcude only
+        else {
+            // exclude
+            combSumHelper(ar, ind+1, tar, res, cur);
+        }
+    }
+    
+    void combSumHelper2(vector<int> ar, int start, int tar, vector<vector<int>>& res, vector<int> cur = {}) {
+        if(tar == 0) { 
+            res.push_back(cur);
+            return;
+        }
+
+        for (int i = start; i < ar.size(); i++) {
+            if(ar[i] <= tar) {
+                cur.push_back(ar[i]);
+                combSumHelper2(ar, i, tar-ar[i], res, cur);
                 cur.pop_back();   
             }
         }
-        return;
-
     }
-    vector<vector<int>> combinationSum(vector<int>& can, int target) {
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
+        combSumHelper2(candidates, 0, target, res);
         
-        subset_sum(can, target, res);
         return res;
     }
 };
