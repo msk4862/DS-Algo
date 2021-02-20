b7c3cf3599c6d4dea568c74fb72c6df94f4397e6
msk4862 <msk4862@gmail.com>
2021-02-20 18:22:18 +0530

    palindrome partition optimised

--- Practice/DP/4_MCMProbs/1_partionPalind.cpp
+++ Practice/DP/4_MCMProbs/1_partionPalind.cpp
@@ -15,6 +15,9 @@ bool isP(string s) {
 	return str.compare(s) == 0 ? true: false;
 }
 
+
+    // we have n*n subproblems to solve and each is taking O(n) time
+    // O(n^3)
 int minCuts_td(string str, int i, int j, int dp[][1000]) {
     // cout<<isP(str.substr(i, j-i+1)) <<str.substr(i, j-i+1)<<endl;
     if(i == j || isP(str.substr(i, j-i+1))) return 0;
@@ -45,3 +48,45 @@ int main() {
 
     return 0;
 }
+
+
+// 2nd approach
+// optimised
+class Solution {
+public:
+    
+    bool isP(string s, int st, int end) {
+        while(st < end) {
+            if(s[st] != s[end]) return false;
+            ++st;
+            --end;
+        }
+        return true;
+    }
+
+    int minCuts_td(string str, int i, int j, int dp[][2001]) {
+        if(i == j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(isP(str, i, j)) return dp[i][j] = 0;
+        
+        int min_cuts = INT_MAX;
+        for(int k = i; k < j; ++k) {
+            if(isP(str, i, k)) {
+                int cost = minCuts_td(str, k+1, j, dp) + 1;
+                min_cuts = min(min_cuts, cost);
+            }
+        }
+
+        return dp[i][j] = min_cuts;
+    }
+    
+    int minCut(string s) {
+        int n = s.size();
+
+        int dp[n+1][2001];
+        memset(dp, -1, sizeof(int)*(n+1)*2001);
+
+        return minCuts_td(s, 0, n-1, dp);
+    }
+};
